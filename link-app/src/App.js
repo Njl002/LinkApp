6764d9631f1b4fc3e63d3901e55bc783d39290b8
Njl002 <nathaniellee51@gmail.com>
2020-02-06 18:25:43 -0800

    initial commit

---
+++ link-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from "./Main";
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ link-app/src/constants.js
@@ -0,0 +1,6 @@
+export const navConsts = {
+  HOMEPAGE: "home",
+  PROFILE: "profile",
+  USERPROFILE: "user",
+  MESSAGES: "messages"
+};
