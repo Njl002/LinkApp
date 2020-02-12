b7d432605fcc09dfe9ffaf0b69d953886288c664
Njl002 <nathaniellee51@gmail.com>
2020-02-12 15:13:35 -0800

    baseline css

---
+++ src/components/login/LoginView.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { navConsts } from '../../constants';
+import default_profile_img from '../../resources/blank-profile-picture.png';
+
+export default class LoginView extends Component {
+  render() {
+    return (
+      <div>Login page holder</div>
+    );
+  }
+}
