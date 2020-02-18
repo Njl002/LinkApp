e086fd6bec30a74acda54c14cde8f2b4727de235
Njl002 <nathaniellee51@gmail.com>
2020-02-18 01:29:33 -0800

    uhh reorganize

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const navConsts = {
-  HOMEPAGE: "home",
-  LINKS: "links",
-  LINKPROFILE: "link",
-  USERPROFILE: "user",
-  MESSAGES: "messages",
-  LOGIN: "login",
-  SIGNUP: "signup"
-};
