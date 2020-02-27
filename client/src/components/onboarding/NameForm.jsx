b9e3d49a74840951a77854f90b3622c4b71785c0
Kenta Asai <asai.kenta@gmail.com>
2020-02-26 17:10:34 -0800

    added icons and some styling to nameform.jsx

--- client/src/components/onboarding/NameForm.jsx
+++ client/src/components/onboarding/NameForm.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Button, Container, Row, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { navConsts } from '../../constants';
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import "./css/NameForm.css";
 
 export default class NameForm extends Component {
 
@@ -11,8 +13,8 @@ export default class NameForm extends Component {
       <Container>
         <Row>
           <Link to={"/" + LAUNCH}>
-            <Button variant="primary" type="submit">
-              Back
+            <Button variant="primary" type="submit" bsPrefix="back-button">
+              <FiChevronLeft />
             </Button>
           </Link>
         </Row>
@@ -26,6 +28,7 @@ export default class NameForm extends Component {
                 type="text" 
                 name="firstNameForm" 
                 placeholder="First Name" 
+                bsPrefix="name-form"
                 onChange={e => this.props.onFirstNameChange(e.target.value)}
               />
             </Form.Group>
@@ -38,6 +41,7 @@ export default class NameForm extends Component {
                 type="text" 
                 name="lastNameForm" 
                 placeholder="Last Name"
+                bsPrefix="name-form"
                 onChange={e => this.props.onLastNameChange(e.target.value)}
               />
             </Form.Group>
@@ -45,7 +49,7 @@ export default class NameForm extends Component {
         </Row>
         <Row>
           <Button variant="primary" type="submit" onClick={this.props.onNextClick}>
-            >
+            <FiChevronRight />
           </Button>
         </Row>
       </Container>
